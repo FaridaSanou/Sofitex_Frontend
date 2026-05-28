81803c9cd3822b0eeaf74697cef85bee78db2d9e
loriekanse66 <loriekanse66@gmail.com>
2026-05-28 09:41:12 +0000

    Incription et connexion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import CreateAccount from "./pages/CreateAccount"
-import AdminDashboard from "./components/AdminDashboard"
+import Login          from "./pages/Login"
+import CreateAccount  from "./pages/CreateAccount"
+import AdminDashboard from "./pages/AdminDashboard"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/jdfkd" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<CreateAccount />} />
-        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/"               element={<Login />} />
+        <Route path="/login"          element={<Login />} />
+        <Route path="/register"       element={<CreateAccount />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   )
