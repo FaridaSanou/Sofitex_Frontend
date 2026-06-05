b9de47847085c6de708573ac50e439165087f4ee
loriekanse66 <loriekanse66@gmail.com>
2026-06-05 09:10:16 +0000

    recuperer

--- src/services/api.js
+++ src/services/api.js
@@ -14,7 +14,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
     (res) => res,
     (err) => {
-        if (err.response ? .status === 401) {
+        if (err.response && err.response.status === 401) {
             localStorage.removeItem("token");
             localStorage.removeItem("role");
             window.location.href = "/";
